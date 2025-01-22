b3dfef29796ca502faf411011e6adb26a61a3eb2
Emre Tufan <emretufan42@gmail.com>
2025-01-23 01:44:48 +0300

    feat: add favicon and manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Ubuntu_Mono } from "next/font/google";
 import "./globals.css";
+import { Fragment } from "react";
 
 const ubuntuMono = Ubuntu_Mono({
   subsets: ["latin"],
@@ -10,9 +11,42 @@ const ubuntuMono = Ubuntu_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Emre Tufan",
+  title: "Mustafa Emre Tufan",
   description: "Frontend Developer",
 };
+
+const Favicon = () => {
+  return (
+    <Fragment>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/favicons/android-chrome-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <meta name="theme-color" content="#0A0A0A" />
+    </Fragment>
+  );
+};
+
 
 export default function RootLayout({
   children,
@@ -21,6 +55,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Favicon />
+      </head>
       <body className={`${ubuntuMono.variable} antialiased font-mono`}>{children}</body>
     </html>
   );
